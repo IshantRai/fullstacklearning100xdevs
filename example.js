f315dfc9d3693a3b6452dacb70cd3bce8915e283
IshantRai <119950690+IshantRai@users.noreply.github.com>
2026-01-06 23:31:35 +0530

    6JAN

--- example.js
+++ example.js
@@ -15,4 +15,13 @@ console.log(ans);
     if (allPeople[i].gender === "male") {   
         console.log(allPeople[i].firstName);
     }
+
+let array = [1, 2, 3, 4, 5];
+let reversedArray = [];
+for (let i = array.length - 1; i >= 0; i--) {
+    reversedArray.push(array[i]);
 }
+console.log(reversedArray);
+
+
+// expected output - [5, 4, 3, 2, 1]    
