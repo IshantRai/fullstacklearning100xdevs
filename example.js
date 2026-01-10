df2f3f85b60a0978d461709a43d7edc966a19ca6
IshantRai <119950690+IshantRai@users.noreply.github.com>
2026-01-10 23:30:33 +0530

    10JAN

--- example.js
+++ example.js
@@ -7,14 +7,25 @@ console.log(sum(10, 20)); // 30
 
 // End of the code
 
-let ans = 0;
-for (let i = 0; i <= 100; i++) {
-    ans += i;
-}
-console.log(ans);
-    if (allPeople[i].gender === "male") {   
-        console.log(allPeople[i].firstName);
-    }
+// let ans = 0;
+// for (let i = 0; i <= 100; i++) {
+//     ans += i;
+// }
+// console.log(ans);
+//     if (allPeople[i].gender === "male") {   
+//         console.log(allPeople[i].firstName);
+//     }
+
+// let array = [1, 2, 3, 4, 5];
+// let reversedArray = [];
+// for (let i = array.length - 1; i >= 0; i--) {
+//     reversedArray.push(array[i]);
+// }
+// console.log(reversedArray);
+
+
+// expected output - [5, 4, 3, 2, 1]    
+
 
 let array = [1, 2, 3, 4, 5];
 let reversedArray = [];
@@ -22,6 +33,3 @@ for (let i = array.length - 1; i >= 0; i--) {
     reversedArray.push(array[i]);
 }
 console.log(reversedArray);
-
-
-// expected output - [5, 4, 3, 2, 1]    
