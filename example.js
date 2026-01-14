40058eff457e751795717f249724aeb9063ddef4
IshantRai <119950690+IshantRai@users.noreply.github.com>
2026-01-14 21:10:18 +0530

    14JAN

--- example.js
+++ example.js
@@ -1,51 +1,63 @@
-//hii this is only for commiting code online 
-function sum (a, b) {
+// //hii this is only for commiting code online 
+// function sum (a, b) {
+//     return a + b;
+// }
+// console.log(sum(4, 8)); // 12
+// console.log(sum(10, 20)); // 30
+
+// // End of the code
+
+// // let ans = 0;
+// // for (let i = 0; i <= 100; i++) {
+// //     ans += i;
+// // }
+// // console.log(ans);
+// //     if (allPeople[i].gender === "male") {   
+// //         console.log(allPeople[i].firstName);
+// //     }
+
+// // let array = [1, 2, 3, 4, 5];
+// // let reversedArray = [];
+// // for (let i = array.length - 1; i >= 0; i--) {
+// //     reversedArray.push(array[i]);
+// // }
+// // console.log(reversedArray);
+
+
+// // expected output - [5, 4, 3, 2, 1]    
+
+
+// // let array = [1, 2, 3, 4, 5];
+// // let reversedArray = [];
+// // for (let i = array.length - 1; i >= 0; i--) {
+// //     reversedArray.push(array[i]);
+// // }
+// // console.log(reversedArray);
+
+// let array = [1, 2, 3, 4, 5];
+// let reversedArray = [];
+// for (let i = array.length - 1; i >= 0; i--) {
+//     reversedArray.push(array[i]);
+// }
+// console.log(reversedArray);
+
+
+// expected output - [5, 4, 3, 2, 1]  
+
+
+// function sum(num1, num2, fnToCall) {
+//     let result = num1 + num2;
+//     fnToCall(result);
+// }
+
+// //exmp 2 
+// passing fun insde a fun and doing callback
+
+function Calculate(a, b, operationFunction){
+    return operationFunction(a, b);
+}
+function sum(a, b){
     return a + b;
 }
-console.log(sum(4, 8)); // 12
-console.log(sum(10, 20)); // 30
-
-// End of the code
-
-// let ans = 0;
-// for (let i = 0; i <= 100; i++) {
-//     ans += i;
-// }
-// console.log(ans);
-//     if (allPeople[i].gender === "male") {   
-//         console.log(allPeople[i].firstName);
-//     }
-
-// let array = [1, 2, 3, 4, 5];
-// let reversedArray = [];
-// for (let i = array.length - 1; i >= 0; i--) {
-//     reversedArray.push(array[i]);
-// }
-// console.log(reversedArray);
-
-
-// expected output - [5, 4, 3, 2, 1]    
-
-
-// let array = [1, 2, 3, 4, 5];
-// let reversedArray = [];
-// for (let i = array.length - 1; i >= 0; i--) {
-//     reversedArray.push(array[i]);
-// }
-// console.log(reversedArray);
-
-let array = [1, 2, 3, 4, 5];
-let reversedArray = [];
-for (let i = array.length - 1; i >= 0; i--) {
-    reversedArray.push(array[i]);
-}
-console.log(reversedArray);
-
-
-expected output - [5, 4, 3, 2, 1]  
-
-
-function sum(num1, num2, fnToCall) {
-    let result = num1 + num2;
-    fnToCall(result);
-}
+const result = Calculate(4, 2, sum);
+console.log(result);  // 6
