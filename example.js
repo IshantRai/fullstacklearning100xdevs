8e9c20d2e0ff7854e046a235b962a3f1b2126b00
IshantRai <119950690+IshantRai@users.noreply.github.com>
2026-01-18 23:12:22 +0530

    18JAN

--- example.js
+++ example.js
@@ -104,3 +104,11 @@ for (let i =0 ; i < allPeople.length; i++) {
         console.log(allPeople[i]["firstName"]);
     }
 }
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 === 0){
+        console.log(arr[i]);
+    }
+}
