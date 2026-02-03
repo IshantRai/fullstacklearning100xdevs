15c298d5d03c9e7587564e49334903ac2b77c8a3
IshantRai <119950690+IshantRai@users.noreply.github.com>
2026-02-03 19:07:08 +0530

    03FEB

--- example.js
+++ example.js
@@ -161,9 +161,18 @@
 // }
 
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-for (let i = 0; i < arr.length; i++) {
-    if(arr[i] % 2 === 0){
-        console.log(arr[i]);
-    }
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] % 2 === 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+
+const peopleArray = ["rahul", "rohit", "neha", "priya", "sonal"];
+const genderArray = ["male", "male", "female", "female", "male"];
+for (let i = 0; i < peopleArray.length; i++) {
+    if (genderArray[i] === "male") {
+        console.log(peopleArray[i]);
+    }   
 }
